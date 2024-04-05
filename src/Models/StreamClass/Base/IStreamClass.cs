5abec88ed382135e1dd4f779c58b704f0c08698b
Vitalii Savitskii <visa@ecco.com>
2024-04-05 11:15:14 +0200

    Add the StreamClass models (#9)

---
+++ src/Models/StreamClass/Base/IStreamClass.cs
@@ -0,0 +1,16 @@
+﻿using k8s;
+using k8s.Models;
+
+namespace Arcane.Operator.Models.StreamClass.Base;
+
+/// <summary>
+/// Base interface for StreamClass objects
+/// </summary>
+public interface IStreamClass: IKubernetesObject<V1ObjectMeta> 
+{
+    /// <summary>
+    /// Return Unique ID for the StreamClass object
+    /// </summary>
+    /// <returns></returns>
+    string ToStreamClassId();
+}
