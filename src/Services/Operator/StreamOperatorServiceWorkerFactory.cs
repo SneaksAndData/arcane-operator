8e67a79d9381f7a98b98224a5734b0b4209416c1
Vitalii Savitskii <visa@ecco.com>
2024-05-01 14:16:13 +0200

    Remove unused generic type arguments from StreamOperatorService (#58)
    * Remove unused generic type arguments from StreamOperatorService
    
    * Fix formatting issues

--- src/Services/Operator/StreamOperatorServiceWorkerFactory.cs
+++ src/Services/Operator/StreamOperatorServiceWorkerFactory.cs
@@ -6,7 +6,6 @@ using Arcane.Operator.Models.StreamDefinitions;
 using Arcane.Operator.Services.Base;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Snd.Sdk.Kubernetes.Base;
 
 namespace Arcane.Operator.Services.Operator;
 
@@ -15,17 +14,16 @@ public class StreamOperatorServiceWorkerFactory : IStreamOperatorServiceWorkerFa
 {
     private readonly ILoggerFactory loggerFactory;
     private readonly IMaterializer materializer;
-    private readonly IKubeCluster kubeCluster;
     private readonly IStreamingJobOperatorService jobOperatorService;
     private readonly IStreamDefinitionRepository streamDefinitionRepository;
 
-    public StreamOperatorServiceWorkerFactory(ILoggerFactory loggerFactory, IMaterializer materializer,
-        IKubeCluster kubeCluster, IStreamingJobOperatorService jobOperatorService,
+    public StreamOperatorServiceWorkerFactory(ILoggerFactory loggerFactory,
+        IMaterializer materializer,
+        IStreamingJobOperatorService jobOperatorService,
         IStreamDefinitionRepository streamDefinitionRepository)
     {
         this.loggerFactory = loggerFactory;
         this.materializer = materializer;
-        this.kubeCluster = kubeCluster;
         this.jobOperatorService = jobOperatorService;
         this.streamDefinitionRepository = streamDefinitionRepository;
     }
@@ -33,12 +31,11 @@ public class StreamOperatorServiceWorkerFactory : IStreamOperatorServiceWorkerFa
     /// <inheritdoc cref="IStreamOperatorServiceWorkerFactory.Create"/>
     public StreamOperatorServiceWorker Create(IStreamClass streamClass)
     {
-        var streamOperatorService = new StreamOperatorService<StreamDefinition>(
-            this.kubeCluster,
+        var streamOperatorService = new StreamOperatorService(
             streamClass,
             this.jobOperatorService,
             this.streamDefinitionRepository,
-            this.loggerFactory.CreateLogger<StreamOperatorService<StreamDefinition>>()
+            this.loggerFactory.CreateLogger<StreamOperatorService>()
         );
         return new StreamOperatorServiceWorker(
             this.loggerFactory.CreateLogger<StreamOperatorServiceWorker>(),
