7ee31eaf2a767a1b7bf4332f2183a0c8dcfccce5
Jakob Roar Bentzon <jakob.bentzon@omnigraph.io>
2025-02-09 12:13:49 +0000

    Allow Overriding Minimum LogLevels from Chart Values

--- src/Configurations/LoggingConfiguration.cs
+++ src/Configurations/LoggingConfiguration.cs
@@ -14,4 +14,15 @@ public class LoggingConfiguration
     /// Custom static properties to enrich the logger with.
     /// </summary>
     public Dictionary<string, string> CustomProperties { get; init; }
+
+    /// <summary>
+    /// Minimum level overrides for specific loggers.
+    /// <example>
+    /// {
+    ///   "Microsoft": "Warning",
+    ///   "System": "Warning"
+    /// }
+    /// </example>
+    /// </summary>
+    public Dictionary<string, string> MinimumLevelOverrides { get; init; }
 };
