a358bf9e6d0155e539dfaa6b02139e07377dce1f
Vitalii Savitskii <visa@ecco.com>
2025-12-17 14:48:50 +0100

    Minor fixes

--- pkg/apis/streaming/v1/stream_class_extensions.go
+++ pkg/apis/streaming/v1/stream_class_extensions.go
@@ -1,12 +1,11 @@
 package v1
 
 import (
-	"fmt"
 	"github.com/SneaksAndData/arcane-operator/services/controllers/common"
 )
 
 var _ common.StreamClassWorkerIdentity = (*StreamClass)(nil)
 
 func (in *StreamClass) WorkerId() common.WorkerId {
-	return common.WorkerId(fmt.Sprintf("%s-%s", in.Namespace, in.Name))
+	return common.WorkerId(in.Name)
 }
--- services/controllers/common/worker_identity.go
+++ services/controllers/common/worker_identity.go
@@ -1,7 +1,10 @@
 package common
 
+// WorkerId defines a unique identifier for a stream operator worker
+// Typically this would be derived from the name/namespace of the object being watched
 type WorkerId string
 
+// StreamClassWorkerIdentity defines an interface for objects that can provide a WorkerId
 type StreamClassWorkerIdentity interface {
 	WorkerId() WorkerId
 }
