1e3ef80aa63c9d6ca3782b0020aebe6bcffe94e0
Vitalii Savitskii <visa@ecco.com>
2026-01-07 11:42:53 +0100

    Fixes

--- services/job_configurator_provider.go
+++ services/job_configurator_provider.go
@@ -1,6 +1,8 @@
 package services
 
+// JobConfiguratorProvider defines an interface for types that can provide a JobConfigurator.
 type JobConfiguratorProvider interface {
-	// JobConfigurator returns a JobConfigurator for the stream definition.
+
+	// JobConfigurator returns a JobConfigurator for the current instance.
 	JobConfigurator() JobConfigurator
 }
