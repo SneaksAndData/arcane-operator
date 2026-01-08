9c6af866d43bace813eb3c7a20bd61fa7c8d6a9f
Vitalii Savitskii <visa@ecco.com>
2026-01-08 13:21:50 +0100

    Implemented the stream reconciler state machine (#177)
    * Implemented the stream reconciler state machine
    
    * Review fixes

---
+++ services/controllers/stream/streaming_job.go
@@ -0,0 +1,29 @@
+package stream
+
+import (
+	"fmt"
+	v1 "k8s.io/api/batch/v1"
+)
+
+type StreamingJob v1.Job
+
+func (job StreamingJob) CurrentConfiguration() (string, error) {
+	value, ok := job.Annotations["configuration-hash"]
+	if !ok {
+		return "", fmt.Errorf("job does not contain configuration hash")
+	}
+	return value, nil
+}
+
+func (job StreamingJob) IsCompleted() bool {
+	return job.Status.Succeeded > 0
+}
+
+func (job StreamingJob) IsFailed() bool {
+	return job.Status.Failed > 0
+}
+
+func (job StreamingJob) ToV1Job() *v1.Job {
+	j := v1.Job(job)
+	return &j
+}
