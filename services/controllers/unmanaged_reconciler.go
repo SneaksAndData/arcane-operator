ebbe74f553ffc75510757cf5870d6cd753a1aca4
Vitalii Savitskii <visa@ecco.com>
2026-01-09 10:39:30 +0100

    Add a couple of docstrings for the PR

--- services/controllers/unmanaged_reconciler.go
+++ services/controllers/unmanaged_reconciler.go
@@ -8,7 +8,15 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
+// UnmanagedReconciler is an extension of the standard Reconciler interface
+// that includes a method for setting up unmanaged controllers.
+// Unmanaged controllers are not automatically started by the manager.
+// See: https://pkg.go.dev/sigs.k8s.io/controller-runtime/pkg/controller#NewUnmanaged
+// and https://github.com/kubernetes-sigs/controller-runtime/issues/730
+// for more details.
 type UnmanagedReconciler interface {
 	reconcile.Reconciler
+
+	// SetupUnmanaged sets up the unmanaged controller with the provided cache, scheme, and REST mapper.
 	SetupUnmanaged(cache cache.Cache, scheme *runtime.Scheme, mapper meta.RESTMapper) (controller.Controller, error)
 }
