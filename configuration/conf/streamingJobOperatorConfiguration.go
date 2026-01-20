88fa32a565e1a7cbe939c904f769ea38890ce84c
Vitalii Savitskii <visa@ecco.com>
2026-01-20 15:52:30 +0100

    Remove unused configurations

--- configuration/conf/streamingJobOperatorConfiguration.go
+++
@@ -1,8 +0,0 @@
-package conf
-
-// StreamingJobOperatorConfiguration holds configuration for StreamOperatorService.
-type StreamingJobOperatorConfiguration struct {
-
-	// MaxBufferCapacity is the max buffer capacity for StreamClasses events stream.
-	MaxBufferCapacity int
-}
