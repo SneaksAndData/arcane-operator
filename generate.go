77194e6185938fb9cade742563af1f1fcb35c3a2
Vitalii Savitskii <visa@ecco.com>
2026-01-30 09:51:07 +0100

    Fix unit tests

--- generate.go
+++ generate.go
@@ -4,3 +4,4 @@ package main
 //go:generate mockgen -destination=./tests/mocks/unmanaged_reconciler.go -package=mocks github.com/SneaksAndData/arcane-operator/services/controllers UnmanagedReconciler
 //go:generate mockgen -destination=./tests/mocks/controller.go -package=mocks sigs.k8s.io/controller-runtime/pkg/controller Controller
 //go:generate mockgen -destination=./tests/mocks/job_builder.go -package=mocks github.com/SneaksAndData/arcane-operator/services/controllers/stream JobBuilder
+//go:generate mockgen -destination=./tests/mocks/job_mock/secret_reference_provider.go -package=mocks github.com/SneaksAndData/arcane-operator/services/job SecretReferenceProvider
