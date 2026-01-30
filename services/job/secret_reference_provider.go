e489b60fed9e2cec8ae5218d7d266239732171af
Vitalii Savitskii <visa@ecco.com>
2026-01-30 09:34:53 +0100

    Fix

--- services/job/secret_reference_provider.go
+++ services/job/secret_reference_provider.go
@@ -2,6 +2,9 @@ package job
 
 import corev1 "k8s.io/api/core/v1"
 
+// SecretReferenceProvider defines an interface for types that can provide a reference to a Kubernetes Secret.
 type SecretReferenceProvider interface {
+
+	// GetReferenceForSecret retrieves a LocalObjectReference for the specified secret name.
 	GetReferenceForSecret(name string) (*corev1.LocalObjectReference, error)
 }
