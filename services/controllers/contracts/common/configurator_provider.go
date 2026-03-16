6610b5ecf53dfa9a9465d6f87905c57a7daab983
Vitalii Savitskii <visa@ecco.com>
2026-03-16 08:44:14 +0100

    Add backfill properties to the job (#262)

--- services/controllers/contracts/common/configurator_provider.go
+++ services/controllers/contracts/common/configurator_provider.go
@@ -21,13 +21,19 @@ func NewConfiguratorProvider(u *unstructured.Unstructured, parent stream.Definit
 }
 
 func (c *ConfiguratorProvider) JobConfigurator() (job.Configurator, error) { // coverage-ignore (should be tested in integration tests)
-	configurator := job.NewConfiguratorChainBuilder().
+	// For job streams this configurator always emit a non-backfill configurator
+	// which could be overridden by backfill request later in the reconciliation loop if BFR is provided.
+	// For cronjob streams this configurator always emit a backfill configurator since cronjob stream only supports
+	// backfill mode.
+	alwaysBackfill := c.parent.GetBackend() == stream.CronJob
+
+	configuratorBuilder := job.NewConfiguratorChainBuilder().
 		WithConfigurator(job.NewNameConfigurator(c.underlying.GetName())).
 		WithConfigurator(job.NewNamespaceConfigurator(c.underlying.GetNamespace())).
 		WithConfigurator(job.NewMetadataConfigurator(c.underlying.GetName(), c.underlying.GetKind())).
-		WithConfigurator(job.NewBackfillConfigurator(false)).
+		WithConfigurator(job.NewBackfillConfigurator(alwaysBackfill)).
 		WithConfigurator(job.NewEnvironmentConfigurator(c.underlying.Object["spec"], "SPEC")).
-		WithConfigurator(job.NewOwnerConfigurator(c.parent.ToOwnerReference())).
-		Build()
-	return configurator, nil
+		WithConfigurator(job.NewOwnerConfigurator(c.parent.ToOwnerReference()))
+
+	return configuratorBuilder.Build(), nil
 }
