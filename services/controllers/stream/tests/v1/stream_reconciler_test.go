0001f63c034479e5826b695ce06d74168eca176c
Vitalii Savitskii <visa@ecco.com>
2026-05-20 13:31:11 +0200

    Finally fix unit tests

--- services/controllers/stream/tests/v1/stream_reconciler_test.go
+++ services/controllers/stream/tests/v1/stream_reconciler_test.go
@@ -713,8 +713,20 @@ func Test_UpdatePhase_Scheduled_to_Scheduled_no_cron_job(t *testing.T) {
 
 func Test_UpdatePhase_Scheduled_to_Scheduled_recreate_cron_job(t *testing.T) {
 	// Arrange
-	builder := v3.NewMockStreamDefinitionBuilder(objectName).WithPhase(stream.Scheduled).WithSuspendedSpec(false).WithSchedule("* * * * *")
+	builder := v3.NewMockStreamDefinitionBuilder(objectName).
+		WithPhase(stream.Scheduled).
+		WithSuspendedSpec(false).
+		WithSchedule("* * * * *")
 	k8sClient := helpers.SetupClientFromBuilders(nil, builder, helpers.NewFakeClientResourcesBuilder().WithOutdatedCronJob(objectName))
+
+	u, err := helpers.GetStreamDefinitionUnstructured(t.Context(), k8sClient, objectName, helpers.GroupVersionKindV2)
+	require.NoError(t, err)
+
+	def, err := contracts.FromUnstructured(u)
+	require.NoError(t, err)
+
+	definitionHash, err := def.CurrentConfiguration(nil)
+	require.NoError(t, err)
 
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
@@ -732,7 +744,7 @@ func Test_UpdatePhase_Scheduled_to_Scheduled_recreate_cron_job(t *testing.T) {
 		require.Equal(t, "* * * * *", cj.Spec.Schedule)
 		require.Equal(t, batchv1.ForbidConcurrent, cj.Spec.ConcurrencyPolicy)
 		// computed manually for the test definition
-		require.Equal(t, "f64da5796994069c5b50e98041dd9d2f", cj.Annotations["configuration-hash"])
+		require.Equal(t, definitionHash, cj.Annotations["configuration-hash"])
 	})
 }
 
